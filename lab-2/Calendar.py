0b9a85e7145d5a11fffffdbe58a6f063d611e798
dariy-vel <dariy.velychko@gmail.com>
2019-09-19 08:58:48 +0300

    Removed useless checks and actions

--- lab-2/Calendar.py
+++ lab-2/Calendar.py
@@ -8,8 +8,6 @@ class Calendar:
             return
         if len(self.output_list) == 0:
             self.output_list.append(input_tuple)
-        elif self.output_list[-1][0] <= input_tuple[1] < self.output_list[-1][1]:
-            self.output_list[-1] = (input_tuple[0], self.output_list[-1][1])
         elif input_tuple[0] <= self.output_list[-1][1] < input_tuple[1]:
             self.output_list[-1] = (self.output_list[-1][0], input_tuple[1])
         else:
