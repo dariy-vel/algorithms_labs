601947b00be59a89e179939a067882b4452ee472
dariy-vel <dariy.velychko@gmail.com>
2020-06-04 11:07:24 +0300

    fixed lab_5 - fantz, dynamic programming

--- lab_5/main.py
+++ lab_5/main.py
@@ -2,18 +2,20 @@ def angry_beaver(binary_in, x):
     binary_x = []
     power = 0
     insert_string = ""
+
     while len(insert_string) <= len(binary_in):
         insert_string = "{0:b}".format(pow(x, power))
         binary_x.append(insert_string)
         power += 1
 
+    arr = []
+
     def solution(input, counter=0):
-        arr = []
         for i in range(0, len(binary_in)):
             if input[i:] in binary_x:
-                counter += 1
-                arr.append(solution(input[:i], counter))
-        return 1
+                counter = solution(input[:i], counter + 1)
+        arr.append(counter)
+        return min(arr)
 
     return solution(binary_in)
 
